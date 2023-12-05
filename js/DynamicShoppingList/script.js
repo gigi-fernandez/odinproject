62422dd500435ec9adc86d63ac0749a1e2927250
Guillermo-Fernandez-Gonzalez <guillermo.ossu@proton.me>
2023-12-05 17:14:54 +0100

    dynamic shopping list

---
+++ js/DynamicShoppingList/script.js
@@ -0,0 +1,54 @@
+/**
+ * Action plan
+ * 
+ * Whenever the Add item button is pressed (1), you select the
+ * content of 'input' (2) and add it to the list along with the
+ * 'delete' button (3).
+ * 
+ * Whenever the delete button is pressed, it's associated
+ * list item is removed, as well as itself. (4)
+ * 
+ * (3) CreateListItem
+ * (4) DeleteListItem
+*/
+
+function CreateListItem(itemName) {
+  const li = document.createElement("li");
+  li.textContent = itemName;
+  
+  const deleteButton = document.createElement("button")
+  deleteButton.textContent = 'Delete';
+  deleteButton.classList.toggle('delete')
+  deleteButton.style.display = "inline"
+  deleteButton.addEventListener('click', (ev)=>{
+    if (ev.target != deleteButton) return
+    DeleteListItem(li)
+  })
+  
+  li.appendChild(deleteButton)
+  return li;
+}
+function DeleteListItem(itemButton) {
+  itemButton.remove()
+}
+
+const list = document.querySelector('ul');
+const input = document.querySelector('#item');
+const addButton = document.querySelector('.add-item > button');
+
+function AddItem(){
+  const itemName = input.value.trim();
+  input.value = '';
+  const li = CreateListItem(itemName);
+  list.appendChild(li);
+}
+addButton.addEventListener('click', (ev)=>{
+  if (!input.value) return
+  AddItem();
+})
+
+input.addEventListener('keypress', (ev)=>{
+  if (ev.key != "Enter") return
+  if (!input.value) return
+  AddItem();
+})
